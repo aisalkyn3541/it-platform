7aff3fa7bf392a984235edc57773db6ff4cd0f1d
aisalkyn3541 <126472043+aisalkyn3541@users.noreply.github.com>
2023-09-09 16:32:49 +0600

    header and footer (#1)
    Co-authored-by: Aisalkyn <aisalkyn3541@gmail.com>

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,49 @@
-import React from 'react'
-
-const Header = () => {
+import React from "react";
+import { NavLink, Link } from 
+"react-router-dom";
+import logo from "../../img/логотип.svg";
+import './index.scss'
+function Header() {
   return (
-    <div></div>
-  )
+    <div id="header">
+      <div className="container">
+        <div className="header">
+          <div className="header__logo">
+            <NavLink to={"/"} className="header__menu-link">
+              <img src={logo} alt="logo" />
+            </NavLink>
+            <nav className="header__nav">
+              <ul className="header__menu-list">
+                <li className="header__menu-item">
+                  <NavLink to={"/оШколе"} className="header__menu-link">
+                    О школе
+                  </NavLink>
+                </li>
+                <li className="header__menu-item">
+                  <NavLink to={"/нашиКурсы"} className="header__menu-link">
+                    Наши курсы
+                  </NavLink>
+                </li>
+                <li className="header__menu-item">
+                  <NavLink to={"/оНас"} className="header__menu-link">
+                    О нас
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="header__podpiska">
+            <Link to={"/Login"} className="header__podpiska-voyti">
+              Войти
+            </Link>
+            <Link to={""} className="header__podpiska-podpis">
+              Подписаться
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
